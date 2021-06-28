d215c9a80966a2f30b723477b21182835e312866
Rajiv-Kumar-Singh <rv488448@gmail.com>
2021-06-29 00:40:25 +0530

    adding files

--- app.js
+++ app.js
@@ -15,9 +15,11 @@ const mongodb = require("mongodb");
 // app.use(express.json({extended:false}));
 // module.exports = connectDB;
 
-mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost/data10', {useNewUrlParser: true, useUnifiedTopology: true}); 
+mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost/data10', { useNewUrlParser: true, useUnifiedTopology: true });
 
-// mongoose.connect('mongodb+srv://Rajiv_07:<password>@portfolio.gng9i.mongodb.net/test', {useNewUrlParser: true});
+// mongodb+srv://Rajiv_07:<password>@portfolio.gng9i.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+
+// mongoose.connect('mongodb+srv://Rajiv_07:<password>@@portfolio.gng9i.mongodb.net/test', {useNewUrlParser: true, useUnifiedTopology: true});  this will work om localhost
 
 // mongoose.connection.on('connected',()=>{
 //     console.log('Moongoose is connected!!!!');
@@ -26,10 +28,10 @@ mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost/data10', {useN
 
 // Define mongoose Schema
 var contactSchema = new mongoose.Schema({
-name: String,
-contact: String,
-email: String,
-message: String
+    name: String,
+    contact: String,
+    email: String,
+    message: String
 });
 
 
@@ -40,19 +42,19 @@ var Contact = mongoose.model('Contact', contactSchema);
 
 
 // Express Specific Stuff
-app.use('/static',express.static('static'))    // For serving static files
-app.use(express.urlencoded())           
+app.use('/static', express.static('static'))    // For serving static files
+app.use(express.urlencoded())
 
 
 // Pug Specific Stuff
 app.set('view engine', 'pug')                                  // Set the template engine as pug
-app.set('views',path.join(__dirname,'views'))                 
+app.set('views', path.join(__dirname, 'views'))
 
 
 //ENDPOINTS
-app.get('/',(req,res)=>{
-    const params ={ }
-    res.status(200).render('home.pug',params);
+app.get('/', (req, res) => {
+    const params = {}
+    res.status(200).render('home.pug', params);
 })
 
 // app.get('/contact', (req, res)=>{
@@ -60,26 +62,26 @@ app.get('/',(req,res)=>{
 //     res.status(200).render('./contact.pug',params);
 // })
 
- app.post('/', (req, res)=>{
+app.post('/', (req, res) => {
     var myData = new Contact(req.body);
-    myData.save().then(()=>{
-    //res.send("The data has been saved to the database");
-    res.render('home.pug')
-    }).catch(()=>{
-    res.status(400).send("Item was not saved to the database");
+    myData.save().then(() => {
+        //res.send("The data has been saved to the database");
+        res.render('home.pug')
+    }).catch(() => {
+        res.status(400).send("Error!!, Server connection failed");
     });
     // res.send('Thank you!');
-     // res.status(200).render('contact.pug');
- })
+    // res.status(200).render('contact.pug');
+})
 
-        //  app.use('myData',require('app'));
+//  app.use('myData',require('app'));
 
 
-        // if(process.env.NODE_ENV ==='production') {
-        //     app.use(express.static('build'));
-        // }
+// if(process.env.NODE_ENV ==='production') {
+//     app.use(express.static('build'));
+// }
 
 //Start the server
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`The application started successfully on port ${port}`);
 })
